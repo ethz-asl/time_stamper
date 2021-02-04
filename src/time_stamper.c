abd7d90f44b6ca4354349d6c5f53cecbeb6cfc57
wraith_ethz <matthias.mueller@mavt.ethz.ch>
2021-02-04 16:33:00 +0000

    pin IRQ works, no timestamping made yet

--- src/time_stamper.c
+++ src/time_stamper.c
@@ -3,11 +3,17 @@
 #include <linux/kernel.h>
 #include <linux/kobject.h>
 #include <linux/sizes.h>
+#include <linux/gpio.h>      
+#include <linux/interrupt.h> 
+#include <linux/delay.h>
+#include <linux/time.h>
 
 // Constants
 /*****************************************/
-#define DRV_NAME           "time_stamper"
-#define N_BUFFER_ENTRIES   1024
+#define DRV_NAME            "time_stamper"
+#define N_BUFFER_ENTRIES    1024
+#define IRQ_GPIO            23      // P9_23
+#define TRIGGER_GPIO        27     // P9_27
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Matthias Mueller");
@@ -29,16 +35,32 @@ struct TimestampRingBuffer {
 /*****************************************/
 static ssize_t ts_buffer_readout(struct kobject *kobj, struct kobj_attribute *attr, char *buf);
 static ssize_t ts_buffer_clear(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count);
+static irqreturn_t ts_timestamp_handler(int irq, void* irq_data);
 
 // Global variables
 /*****************************************/
 static struct kobject *ts_kobject;  
 static struct TimestampRingBuffer ringbuffer;
+static unsigned int triggerGPIO = TRIGGER_GPIO;
+static unsigned int irqGPIO = IRQ_GPIO;
+static unsigned int irqNumber;
+static unsigned int toggle = 0;
 // attributes for the sysfs communication, which functions are called by read or write access of the sysfs file
 static struct kobj_attribute ts_buffer_attribute =__ATTR(ts_buffer, 0664, ts_buffer_readout, ts_buffer_clear);
 
 // Main
 /*****************************************/
+
+/**
+ * is called by an GPIO interrupt, und just saves the current time into the buffer
+*/
+static irqreturn_t ts_timestamp_handler(int irq, void* irq_data)
+{
+    toggle = !toggle;
+    gpio_set_value(triggerGPIO, toggle);
+
+    return IRQ_HANDLED;
+}
 
 /**
  * Returns the entries of the buffer (event timestamps) in string format
@@ -69,20 +91,50 @@ static int __init ts_init(void)
     /// create sysfs kernel object folder for the file to access in the file system under /sys/kernel/time_stamper/
     ts_kobject = kobject_create_and_add("time_stamper", kernel_kobj);
     if(!ts_kobject)
-           return -ENOMEM;
+        return -ENOMEM;
 
     /// create the actual file for access the buffer functions (/sys/kernel/time_stamper/ts_buffer)
     err = sysfs_create_file(ts_kobject, &ts_buffer_attribute.attr);
     if (err) {
-           printk(KERN_ALERT DRV_NAME " : failed to create the sysfs file for the buffer access\n");
-           return err;
+        printk(KERN_ALERT DRV_NAME " : failed to create the sysfs file for the buffer access\n");
+        return err;
     }       
+
+    /// setup trigger gpio and set it to output
+    err = gpio_request_one(triggerGPIO, GPIOF_OUT_INIT_LOW, DRV_NAME " trig");
+    if (err < 0) {
+        printk(KERN_ALERT DRV_NAME " : failed to request Trigger pin %d.\n", triggerGPIO);
+        return -1;
+    }
+
+    /// setup irq gpio and set it to input
+    err = gpio_request_one(irqGPIO, GPIOF_IN, DRV_NAME " irq");
+    if (err < 0) {
+        printk(KERN_ALERT DRV_NAME " : failed to request IRQ pin %d.\n", irqGPIO);
+        return -1;
+    }
+
+    /// find out, which IRQ number belongs to the input pin
+    irqNumber = gpio_to_irq(irqGPIO);
+    if (irqNumber < 0) {
+        printk(KERN_ALERT DRV_NAME " : failed to get IRQ for pin %d.\n", irqGPIO);
+        gpio_free(irqGPIO);
+        return -1;
+    }
+
+    /// enable the interrupt as rising edge, connect it to the handler function and send the irqNumber as cookie
+    err = request_any_context_irq(irqNumber, ts_timestamp_handler, IRQF_TRIGGER_RISING, DRV_NAME, (void*)irqNumber);
+    if (err < 0) {
+        printk(KERN_ALERT DRV_NAME " : failed to enable IRQ %d for pin %d.\n",irqNumber, irqGPIO);
+        gpio_free(irqGPIO);
+        return -1;
+    }
 
     /// init the timestamp ring-buffer
     ringbuffer.head = 0;
     ringbuffer.tail = ringbuffer.head;
 
-    printk(KERN_INFO "GPIO IRQ Timestamper initialized and started\n");
+    printk(KERN_INFO DRV_NAME " : GPIO IRQ Timestamper initialized and started\n");
     return 0;
 }
 
@@ -94,7 +146,14 @@ static void __exit ts_exit(void)
     /// remove the kobject and all its sub-files
     kobject_put(ts_kobject);
 
-    printk(KERN_INFO "GPIO IRQ Timestamper ended\n");
+    /// remove all IRQs
+    free_irq(irqNumber, (void*)irqNumber);
+
+    /// remove GPIOs
+    gpio_free(irqGPIO);
+    gpio_free(triggerGPIO);
+
+    printk(KERN_INFO DRV_NAME " : GPIO IRQ Timestamper ended\n");
 }
 
 module_init(ts_init);
