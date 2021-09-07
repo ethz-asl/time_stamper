8993ccf4d9e836cd4ef4c2691e718c7af5e85dc0
Matthias Mueller <matthias.mueller@mavt.ethz.ch>
2021-09-07 16:15:29 +0200

    added open source definition

--- src/time_stamper.c
+++ src/time_stamper.c
@@ -1,3 +1,9 @@
+#if __STDC_VERSION__ >= 199901L
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 500
+#endif /* __STDC_VERSION__ */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
