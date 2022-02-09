66d6188ec917a13a215620bda2de58326781bf2a
4c3y <69460051+4c3y@users.noreply.github.com>
2022-02-09 17:53:56 +0100

    Created subsystem unittests

--- time_stamper_ros/test/test_mock_pwm_subsystem.cc
+++ time_stamper_ros/test/test_mock_pwm_subsystem.cc
@@ -2,8 +2,10 @@
 #include "gmock/gmock.h"
 #include "IPwmSubsystem.h"
 #include "Node.h"
-class MockPwmSubsystem : public IPwmSubsystem {
 
+using ::testing::Return;
+
+class MockPwmSubsystem : public IPwmSubsystem {
  public:
   MOCK_METHOD0(IsExported, bool());
   MOCK_METHOD0(Export, bool());
@@ -18,7 +20,87 @@ class MockPwmSubsystem : public IPwmSubsystem {
   MOCK_METHOD3(Read, bool(const std::string& path, void* buffer, size_t buffer_size));
 };
 
-TEST(MockPwmSubsystem, TestIsExported) {
+TEST(MockPwmSubsystem, TestNodeInitUninitialized) {
+  //Precondition: Subsystem is not enabled and not exported
   MockPwmSubsystem mock_pwm_subsystem;
   Node node(mock_pwm_subsystem);
+
+
+  //Expectation: Export, set frequency to 50 and start subsystem.
+  EXPECT_CALL(mock_pwm_subsystem, IsExported()).Times(1).WillOnce(Return(false));
+  EXPECT_CALL(mock_pwm_subsystem, Export()).WillOnce(Return(true));
+  EXPECT_CALL(mock_pwm_subsystem, SetFrequency(50)).WillOnce(Return(true));
+  EXPECT_CALL(mock_pwm_subsystem, IsRunning()).WillOnce(Return(false));
+  EXPECT_CALL(mock_pwm_subsystem, Start).WillOnce(Return(true));
+
+  node.Init();
 }
+
+TEST(MockPwmSubsystem, TestNodeInitFailExport) {
+  //Precondition: Subsystem is not enabled and not exported
+  MockPwmSubsystem mock_pwm_subsystem;
+  Node node(mock_pwm_subsystem);
+
+  //Expectation: Subsystem fails export.
+  EXPECT_CALL(mock_pwm_subsystem, IsExported()).Times(1).WillOnce(Return(false));
+  EXPECT_CALL(mock_pwm_subsystem, Export()).WillOnce(Return(false));
+
+  node.Init();
+}
+
+TEST(MockPwmSubsystem, TestNodeInitAlreadyExported) {
+  //Precondition: Subsystem is not running but already exported
+  MockPwmSubsystem mock_pwm_subsystem;
+  Node node(mock_pwm_subsystem);
+
+  //Expectation: Subsystem does not export, sets frequency to 50 and starts.
+  EXPECT_CALL(mock_pwm_subsystem, IsExported()).Times(1).WillOnce(Return(true));
+  EXPECT_CALL(mock_pwm_subsystem, SetFrequency(50)).WillOnce(Return(true));
+  EXPECT_CALL(mock_pwm_subsystem, IsRunning()).WillOnce(Return(false));
+  EXPECT_CALL(mock_pwm_subsystem, Start()).WillOnce(Return(true));
+
+  node.Init();
+}
+
+TEST(MockPwmSubsystem, TestNodeInitAlreadyRunning) {
+  //Precondition: Subsystem is exported and already running
+  MockPwmSubsystem mock_pwm_subsystem;
+  Node node(mock_pwm_subsystem);
+
+  // Subsystem sets frequency to 50
+  EXPECT_CALL(mock_pwm_subsystem, IsExported()).Times(1).WillOnce(Return(true));
+  EXPECT_CALL(mock_pwm_subsystem, SetFrequency(50)).WillOnce(Return(true));
+  EXPECT_CALL(mock_pwm_subsystem, IsRunning()).WillOnce(Return(true));
+
+  node.Init();
+}
+
+TEST(MockPwmSubsystem, TestNodeInitFailSetFrequency) {
+  //Precondition: Subsystem is not running but exported
+  MockPwmSubsystem mock_pwm_subsystem;
+  Node node(mock_pwm_subsystem);
+
+  //Expectation: Subsystem fails to set frequency
+  EXPECT_CALL(mock_pwm_subsystem, IsExported()).Times(1).WillOnce(Return(true));
+  EXPECT_CALL(mock_pwm_subsystem, SetFrequency(50)).WillOnce(Return(false));
+
+  node.Init();
+}
+
+TEST(MockPwmSubsystem, TestFrequencyValueValidation) {
+  //Precondition: Subsystem is in any valid state
+  MockPwmSubsystem mock_pwm_subsystem;
+  //TODO define valid range
+
+  //Expectation: Subsystem should filter invalid values
+  EXPECT_CALL(mock_pwm_subsystem, SetFrequency(50)).WillOnce(Return(true));
+  mock_pwm_subsystem.SetFrequency(50);
+}
+
+TEST(MockPwmSubsystem, TestNodeInitSanityCheck) {
+  //Precondition: Subsystem is not exported but running, which is undefined behavior and should stop node instantly
+
+ //TODO implement
+}
+
+
