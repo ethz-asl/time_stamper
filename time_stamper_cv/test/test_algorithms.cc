59d51c4e3cd05f9df316145b03446d2b1dc7c07e
4c3y <69460051+4c3y@users.noreply.github.com>
2022-03-16 16:14:35 +0100

    Added gtest

---
+++ time_stamper_cv/test/test_algorithms.cc
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+#include "opencv2/opencv.hpp"
+
+TEST(algorithm, GetLeftCornerLeds) {
+// TODO Implement
+/*Test values
+X: 191.227 Y: 433.151
+X: 327.372 Y: 428.2
+X: 413.169 Y: 424.062
+X: 496.617 Y: 420.542
+X: 618.001 Y: 417.038
+X: 696.365 Y: 415.483
+X: 773.845 Y: 411.934
+X: 887.715 Y: 409.041
+X: 283.751 Y: 313.101
+X: 323.83 Y: 312.743
+X: 186.286 Y: 314.175
+X: 656.664 Y: 306.679
+186.286314.175
+191.227433.151
+*/
+}
+
+TEST(algorithm, max_yvalues) {
+
+}
