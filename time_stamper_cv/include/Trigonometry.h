d23ce0e1b892fd8b7ed2d63416f5123cc74e623c
4c3y <69460051+4c3y@users.noreply.github.com>
2022-03-31 17:16:59 +0200

    Added docs in Trigonometry.h, made Trigonometry::LawOfCos() private

--- time_stamper_cv/include/Trigonometry.h
+++ time_stamper_cv/include/Trigonometry.h
@@ -3,9 +3,24 @@
 
 class Trigonometry {
  public:
+
+  /**
+   * Calculates distance between two points.
+   * @param a
+   * @param b
+   * @return distance
+   */
   static double CalcDistance(const cv::Point &a, const cv::Point &b);
 
+  /**
+   * Calculates inner angle of point target
+   * @param a
+   * @param b
+   * @param target
+   * @return Inner angle in degrees
+   */
   static double CalcInnerAngle(const cv::Point &a, const cv::Point &b, const cv::Point &target);
 
+ private:
   static double LawOfCos(double a, double b, double c);
 };
